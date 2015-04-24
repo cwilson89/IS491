ad4f883adc27e3f64528d8f1aeafa52351437a50
cwilson89 <wilson89@uab.edu>
2015-04-24 01:46:49 -0500

    Exam 146

---
+++ Exam2/Exam2.js
@@ -0,0 +1,62 @@
+function GetCategories()
+{
+    var objRequest = new XMLHttpRequest();
+    var url = "http://bus-pluto.ad.uab.edu/jsonwebservice/service1.svc/getAllCategories";
+    
+    objRequest.onreadystatechange = function()
+    {
+        if (objRequest.readyState == 4 && objRequest.status == 200)
+        {
+            var output = JSON.parse(objRequest.responseText);
+            GenerateCategoriesOutput(output);
+        }
+    }
+    
+    objRequest.open("GET",url,true);
+    objRequest.send();
+}
+function View()
+{
+    document.getElementById("sec1").style.visibility = visible;
+}
+function About()
+{
+    
+}
+function UpdateCategories()
+{
+    var objRequest = new XMLHttpRequest();
+    var url = "http://bus-pluto.ad.uab.edu/jsonwebservice/service1.svc/getAllCategories";
+    
+    var category = '{"CID":"' + categoryid + '","CDescription":"' + categorydesc + '"}';
+    
+    objRequest.onreadystatechange = function()
+    {
+        if (objRequest.readyState ==4 && objRequest.status == 200)
+        {
+            var result = JSON.parse(objRequest.responseText);
+            GenerateCategoriesOutput(result);
+        }
+    }
+    
+    objRequest.open("POST",url,true)
+    objRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    objRequest.send(category);
+}
+
+function GenerateUpdateOutput()
+{
+    var count = 0;
+    var displaytest = "";
+}
+
+function GenerateCategoriesOutput(result)
+{
+    var count = 0;
+    var displaytext = "";
+    
+    for (count = 0; count <result.GetAllCategoriesResult.length; count++)
+    {
+        
+    }
+}
