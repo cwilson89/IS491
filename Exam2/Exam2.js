c4ec587164b315754eeb666c7f49ea9e2aac3186
Chris Wilson <wilson89@uab.edu>
2015-04-24 23:45:23 -0500

    Final

--- Exam2/Exam2.js
+++ Exam2/Exam2.js
@@ -1,4 +1,4 @@
-function GetCategories()
+function DisplayCategories()
 {
     var objRequest = new XMLHttpRequest();
     var url = "http://bus-pluto.ad.uab.edu/jsonwebservice/service1.svc/getAllCategories";
@@ -7,23 +7,95 @@ function GetCategories()
     {
         if (objRequest.readyState == 4 && objRequest.status == 200)
         {
-            var output = JSON.parse(objRequest.responseText);
-            GenerateCategoriesOutput(output);
+            var result = JSON.parse(objRequest.responseText);
+            var table = document.getElementById("catdisplay");
+            for (count = 1; count <result.GetAllCategoriesResult.length; count++)
+            {
+                
+                var cid = result.GetAllCategoriesResult[count].CID;
+                var name = result.GetAllCategoriesResult[count].CName;
+                var desc = result.GetAllCategoriesResult[count].CDescription;
+                var row = table.insertRow(count);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                cell1.innerHTML = cid;
+                cell2.innerHTML = name;
+                cell3.innerHTML = desc;
+                GenerateCategoriesOutput(output);
+            }
         }
     }
     
     objRequest.open("GET",url,true);
     objRequest.send();
 }
+
+function UpdateCategories()
+{
+    var objRequest = new XMLHttpRequest();
+    var url = "http://bus-pluto.ad.uab.edu/jsonwebservice/service1.svc/updateCatDescription";
+    var cid = document.getElementById("cid3");
+    
+}
+
+function CreateCategories() 
+{
+    var objRequest = new XMLHttpRequest();
+    var url = "http://bus-pluto.ad.uab.edu/jsonwebservice/service1.svc/getAllCategories";
+    
+    var CID = document.getElementById("cid3");
+    var category = document.getElementById("category3");
+    var description = document.getElementById("description3");
+    
+    var newcategory = '{"CName":"' + category + '", "CDescription":"' + description +'"}';
+    
+    objRequest.onreadystatechange = function()
+    {
+        if (objRequest.readyState == 4 && objRequest.status == 200)
+        {
+            var result = JSON.parse(objRequest.responseText);
+            OperationResult(result);
+        }
+    }
+    
+    objRequest.open("GET",url,true);
+    objRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    objRequest.send(newcategory);
+}
+
+function DeleteCategory()
+{
+    var objRequest = new XMLHttpRequest();
+    var url = "http://bus-pluto.ad.uab.edu/jsonwebservice/service1.svc/deleteCategory";    
+    var CID = document.getElementById("cid4");
+    var url=+CID;
+    
+    objRequest.onreadystatechange = function()
+    {
+        if (objRequest.readyState == 4 && objRequest.status == 200)
+        {
+            var result = JSON.parse(objRequest.responseText);
+            GenerateDeleteResult(result);
+        }
+    }
+    
+    objRequest.open("GET",url,true);
+    objRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    objRequest.send();
+}
+
 function View()
 {
     document.getElementById("sec1").style.visibility = visible;
 }
+
 function About()
 {
     
 }
-function UpdateCategories()
+
+function UpdateCategory()
 {
     var objRequest = new XMLHttpRequest();
     var url = "http://bus-pluto.ad.uab.edu/jsonwebservice/service1.svc/getAllCategories";
@@ -32,10 +104,10 @@ function UpdateCategories()
     
     objRequest.onreadystatechange = function()
     {
-        if (objRequest.readyState ==4 && objRequest.status == 200)
+        if (objRequest.readyState == 4 && objRequest.status == 200)
         {
             var result = JSON.parse(objRequest.responseText);
-            GenerateCategoriesOutput(result);
+            GenerateUpdateResult(result);
         }
     }
     
@@ -44,19 +116,51 @@ function UpdateCategories()
     objRequest.send(category);
 }
 
-function GenerateUpdateOutput()
+function GenerateUpdateResult(output)
 {
-    var count = 0;
-    var displaytest = "";
+    if (output.WasSuccessful == 1)
+    {
+        alert("Operation completed successfully.");
+    }
+    if (output.WasSuccessful == 0)
+    {
+        alert("Operation failed with an unspecified error.");
+    }
+    if (output.WasSuccessful == -2)
+    {
+        alert("Operation failed because the data string supplied could not be deserialized into the service object.");
+    }
+    if (output.WasSuccessful == -3)
+    {
+        alert("Operation failed because a record with the supplied Order ID could not be found.");
+    }
 }
 
 function GenerateCategoriesOutput(result)
 {
-    var count = 0;
-    var displaytext = "";
     
-    for (count = 0; count <result.GetAllCategoriesResult.length; count++)
+}
+
+function GenerateCreateResult(output)
+{
+    if (output.WasSuccessful == 1)
     {
-        
+        document.getElementById("result").innerHTML = "The operation was successful!";
+    }
+    if (output.WasSuccessful == 0)
+    {
+        document.getElementById("result").innerHTML = "The operation was not successful!" + "<br>" + output.Exception;
+    }
+}
+
+function GenerateDeleteResult(output)
+{
+    if (output.WasSuccessful == 1)
+    {
+        document.getElementById("result").innerHTML = "The operation was successful!";
+    }
+    if (output.WasSuccessful == 0)
+    {
+        document.getElementById("result").innerHTML = "The operation was not successful!" + "<br>" + output.Exception;
     }
 }
